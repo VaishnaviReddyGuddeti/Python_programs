f293a7a37fc8a199fe9160f3e81e6744a10d7e16
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-07-16 16:06:33 +0530

    del.py

---
+++ Python_Keywords/del.py
@@ -0,0 +1,9 @@
+# to delete an object
+class Myclass:
+    name = " Vaishnavi "
+    del Myclass
+     #print(Myclass)
+    # delete the first item in list
+    x = ["apple", "banana", "cherry"]
+    del x[0]
+    print(x)
