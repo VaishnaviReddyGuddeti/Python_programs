674fae2ee44b641a0702b9549005438f7f0bb48d
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-07-16 08:43:54 +0530

    assert.py

---
+++ Python_Keywords/assert.py
@@ -0,0 +1,6 @@
+# test if  the condition returns true
+x = "Hello"
+# if condition returns true, then nothing happens:
+assert x == "Hello"
+# if condition returns false, then assertion error is raised:
+assert x == "Good Bye"
