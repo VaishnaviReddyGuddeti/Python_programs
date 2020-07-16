2351783e84e69074e993cc2209c91c5e18c40ca9
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-07-16 16:37:43 +0530

    for.py

---
+++ Python_Keywords/for.py
@@ -0,0 +1,9 @@
+# To create a for loop
+# Print each number from 1 to 9:
+for x in range(1, 9):
+  print(x)
+# Loop through all items in a list:
+fruits = ["apple", "banana", "cherry"]
+
+for x in fruits:
+  print(x)
