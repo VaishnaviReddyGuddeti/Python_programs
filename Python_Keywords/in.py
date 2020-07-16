d5d89640a07c0848380511ab2d434e8f0393b97a
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-07-16 16:49:32 +0530

    in.py

---
+++ Python_Keywords/in.py
@@ -0,0 +1,11 @@
+# To check if a value is present in a list, tuple, etc.
+# Check if "banana" is present in the list:
+fruits = ["apple", "banana", "cherry"]
+
+if "banana" in fruits:
+  print("yes")
+# Loop through a list and print the items:
+fruits = ["apple", "banana", "cherry"]
+
+for x in fruits:
+  print(x)
