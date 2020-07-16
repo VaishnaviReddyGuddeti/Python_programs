712b97ca1996438e90b6106af68789ad23f2e24a
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-07-16 16:40:19 +0530

    from.py

---
+++ Python_Keywords/from.py
@@ -0,0 +1,7 @@
+# To import specific parts of a module
+# Import only the time section from the datetime module, and print the time as if it was 15:00:
+from datetime import time
+
+x = time(hour=15)
+
+print(x)
