a11aef1e8912c1042e89256754fe98516cf44cf1
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-07-16 16:13:12 +0530

    else.py

---
+++ Python_Keywords/else.py
@@ -0,0 +1,15 @@
+# Used in conditional statements
+x = 2
+if x > 3:
+  print("YES")
+else:
+  # print("NO")
+# Use the else keyword in a try...except block to define what to do if no errors were raised:
+ x = 5
+
+try:
+  x > 10
+except:
+  print("Something went wrong")
+else:
+  print("The 'Try' code was executed without raising any errors!")
