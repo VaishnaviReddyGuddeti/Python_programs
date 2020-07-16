20cb9b49f5a4c9cfe633cf79bfd1eaada9c42d95
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-07-16 16:04:54 +0530

    elif.py

---
+++ Python_Keywords/elif.py
@@ -0,0 +1,9 @@
+ # The elif keyword is used in conditional statements (if statements), and is short for else if.
+ # Print "YES" if the variable i is a positive number, print "WHATEVER" if i is 0, otherwise print "NO":
+for i in range (-5,5):
+    if i > 0:
+        print("YES")
+    elif i == 0:
+        print("WHATEVER")
+    else:
+        print("NO")
