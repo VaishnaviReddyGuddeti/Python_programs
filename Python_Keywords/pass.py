42cc90fd9b86826ab9de4f51bf1923402e600d15
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-07-16 20:13:46 +0530

    pass.py

---
+++ Python_Keywords/pass.py
@@ -0,0 +1,23 @@
+# A null statement, a statement that will do nothing
+# Create a placeholder for future code:
+for x in [0, 1, 2]:
+  pass
+
+# having an empty for loop like this, would raise an error without the pass statement
+# Using the pass keyword in a function definition:
+def myfunction:
+  pass
+
+# having an empty function definition like this, would raise an error without the pass statement
+# Using the pass keyword in a class definition:
+class Person:
+  pass
+
+# having an empty class definition like this, would raise an error without the pass statement
+# Using the pass keyword in an if statement:
+a = 33
+b = 200
+if b > a:
+  pass
+
+# having an empty if statement like this, would raise an error without the pass statement
