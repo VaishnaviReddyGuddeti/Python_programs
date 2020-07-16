55c9efc04dcdfe1f31574b983d8449f03cadbef5
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-07-16 08:54:04 +0530

    break.py

--- Python_Keywords/break.py
+++ Python_Keywords/break.py
@@ -1,5 +1,12 @@
 # the break keyword is used to break out a 'for' loop and a 'while' loop.
-for i in range(9)
-if i > 3:
-break
-print(i)
+for i in range(9):
+   if i > 3:
+    break
+# print(i)
+i = 1
+while i < 9:
+    print(i)
+    if i == 3:
+        break
+        i += 1
+        
