8f31cc103c595ac095a2a6b1a8d4e4bafd6f876a
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-07-16 16:44:40 +0530

    if.py

---
+++ Python_Keywords/if.py
@@ -0,0 +1,11 @@
+# To make a conditional statement
+# Print "YES" if x larger than 3:
+x = 5
+if x > 3:
+  print("YES")
+# Print "YES" if x is larger than 6, otherwise print "NO
+x = 5
+if x > 6:
+  print("YES")
+else:
+  print("NO")
