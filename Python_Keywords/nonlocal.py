dedfd9920fb643ab8f7e8bc0e79de9c7f675d250
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-07-16 19:58:49 +0530

    nonlocal.py

---
+++ Python_Keywords/nonlocal.py
@@ -0,0 +1,18 @@
+# To declare a non-local variable
+# Make a function inside a function, which uses the variable x as a non local variable:
+def myfunc1():
+  x = "John"
+  def myfunc2():
+    nonlocal x
+    x = "hello"
+  myfunc2()
+  return x
+print(myfunc1())
+# Same example as above, but without the nonlocal keyword:
+def myfunc1():
+  x = "John"
+  def myfunc2():
+    x = "hello"
+  myfunc2()
+  return x
+print(myfunc1())
