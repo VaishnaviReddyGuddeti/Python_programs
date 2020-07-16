98ab72e90f53946e0b744142ae7bc80290aff468
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-07-16 16:31:42 +0530

    false.py

---
+++ Python_Keywords/false.py
@@ -0,0 +1,19 @@
+# Boolean value, result of comparison operations
+# Print the result of the comparison "5 is larger than 6":
+print(5 > 6)
+# Other comparisons that returns False:
+print(5 > 6)
+
+print(4 in [1,2,3])
+
+print("hello" == "goodbye")
+
+print(5 == 6)
+
+print(5 == 6 or 6 == 7)
+
+print(5 == 6 and 6 == 7)
+
+print(not(5 == 5))
+
+print(3 not in [1,2,3])
