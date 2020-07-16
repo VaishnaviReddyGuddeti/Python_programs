5c1f2e8f6eac2587020db080ef6d0612c395c440
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-07-16 20:09:15 +0530

    or.py

---
+++ Python_Keywords/or.py
@@ -0,0 +1,12 @@
+# A logical operator
+# Return True if one of the statements are True:
+x = (5 > 3 or 5 > 10)
+# print(x)
+# Using the or keyword in an if statement:
+if 5 > 3 or 5 > 10:
+  print("At least one of the statements are True")
+else:
+  print("None of the statements are True")
+
+
+​
