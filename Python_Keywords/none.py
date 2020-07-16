c529d3c0b30d19552b2b7101f0fcd5c708ba39ba
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-07-16 19:55:55 +0530

    none.py

---
+++ Python_Keywords/none.py
@@ -0,0 +1,10 @@
+# Represents a null value
+# Assign the value None to a variable:
+x = None
+print(x)
+# If you do a boolean if test, what will happen? Is None True or False:
+x = None
+if x:
+  print("Do you think None is True")
+else:
+  print("None is not True...")
