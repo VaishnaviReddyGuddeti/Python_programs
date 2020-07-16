a488b8db26d299e4cbf2c929cf816d8392ca8bdf
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-07-16 19:53:14 +0530

    lambda.py

---
+++ Python_Keywords/lambda.py
@@ -0,0 +1,7 @@
+# To create an anonymous function
+# Create a function that adds 10 to any number you send:
+x = lambda a : a + 10
+print(x(5))
+# A lambda function with three arguments:
+x = lambda a, b, c : a + b + c
+print(x(5, 6, 3))
