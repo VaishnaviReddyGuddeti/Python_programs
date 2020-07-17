9bd384f5a2594876fc64de6a4d39600d45e46762
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-07-17 22:55:04 +0530

    MultiLine_Strings.py

---
+++ Python_Strings/MultiLine_Strings.py
@@ -0,0 +1,13 @@
+# You can assign a multiline string to a variable by using three quotes:
+# You can use three double quotes:
+a = """ What is your name,
+where do you work,
+what is you income
+what are your dreams."""
+# print(a)
+# or
+a = ''' What is your name,
+where do you work,
+what is you income
+what are your dreams.'''
+print(a)
