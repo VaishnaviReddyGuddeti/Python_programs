2c59533c4f960f8fce66c40b083a6e8aa7431e8c
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-07-17 22:01:48 +0530

    Variablenames.py

---
+++ Python_variables/Variablenames.py
@@ -0,0 +1,16 @@
+# Variable names are case-sensitive (age, Age and AGE are three different variables)
+# A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+
+#Legal variable names:
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+#Illegal variable names:
+2myvar = "John"
+my-var = "John"
+my var = "John"
+# NOTE  : Variable names are case sensitive
