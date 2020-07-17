ee57acb5415cf048ececa61a2112ae87507a4bdb
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-07-17 22:03:20 +0530

    Multi_Line_Comments.py

---
+++ Python_comments/Multi_Line_Comments.py
@@ -0,0 +1,15 @@
+# Python does not really have a syntax for multi line comments.
+
+# To add a multiline comment you could insert a # for each line:
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+# Or, not quite as intended, you can use a multiline string.
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
+# As long as the string is not assigned to a variable, Python will read the code, but then ignore it, and you have made a multiline comment.
