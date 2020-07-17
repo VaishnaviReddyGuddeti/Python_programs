3a5c77f729154e163c9220f6182d34a675035fa8
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-07-17 22:31:48 +0530

    Specify_VariableType.py

---
+++ Python_Casting/Specify_VariableType.py
@@ -0,0 +1,13 @@
+# Casting in python is therefore done using constructor functions:
+
+# 1.int() - constructs an integer number from an integer literal, a float literal , or a string literal
+# 2.float() - constructs a float number from an integer literal, a float literal or a string literal
+# 3. str() - constructs a string from a wide variety of data types, including strings, integer literals and float literals
+
+# INTEGERS :
+x = int(1)
+y = int(2.8)
+z = int("3")
+print(x)
+print(y)
+print(z)
