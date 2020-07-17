c65cf15ef44ab135bad1abaa5b9f7d22793eabf1
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-07-17 23:02:55 +0530

    Negative_indexing.py

---
+++ Python_Strings/Negative_indexing.py
@@ -0,0 +1,5 @@
+# Use negative indexes to start the slice from the end of the string:
+
+# Get the characters from position 5 to position 1, starting the count from the end of the string:
+b = "Hello, World!"
+print(b[-5:-2])
