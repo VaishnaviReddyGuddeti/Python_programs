886e4458283ff4cd7028b803e437c00e6f3a453c
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-07-17 22:00:44 +0530

    Global_variables.py

---
+++ Python_variables/Global_variables.py
@@ -0,0 +1,29 @@
+# Variables that are created outside of a function (as in all of the examples above) are known as global variables.
+# Global variables can be used by everyone, both inside of functions and outside.
+
+# Create a variable outside of a function, and use it inside the function
+x = "awesome"
+def myfunc():
+  print("Python is " + x)
+myfunc()
+# Create a variable inside a function, with the same name as the global variable
+x = "awesome"
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+myfunc()
+print("Python is " + x)
+# If you use the global keyword, the variable belongs to the global scope:
+def myfunc():
+  global x
+  x = "fantastic"
+myfunc()
+print("Python is " + x)
+# To change the value of a global variable inside a function, refer to the variable by using the global keyword:
+x = "awesome"
+
+def myfunc():
+  global x
+  x = "fantastic"
+myfunc()
+print("Python is " + x)
