5cd6ff9ba22024a2e494a8644d6e05efa426982a
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-07-17 22:40:53 +0530

    Strings_Literals.py

---
+++ Python_Strings/Strings_Literals.py
@@ -0,0 +1,10 @@
+# String literals in python are surrounded by either single quotation marks, or double quotation marks.
+
+# 'hello' is the same as "hello".
+
+# You can display a string literal with the print() function:
+
+#You can use double or single quotes:
+
+print("Hello")
+print('Hello')
