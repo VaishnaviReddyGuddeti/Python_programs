6d699dc90dd9e20973320b3bedfd47fa07854d8b
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-07-18 22:45:07 +0530

    Find().py

---
+++ Python_String_Methods/Find().py
@@ -0,0 +1,28 @@
+# find() - Searches the string for a specified value and returns the position of where it was found
+
+# Where in the text is the word "welcome"?:
+txt = "Hello, welcome to my world."
+
+x = txt.find("welcome")
+
+print(x)
+
+# Where in the text is the first occurrence of the letter "e"?:
+txt = "Hello, welcome to my world."
+
+x = txt.find("e")
+
+print(x)
+
+# Where in the text is the first occurrence of the letter "e" when you only search between position 5 and 10?:
+txt = "Hello, welcome to my world."
+
+x = txt.find("e", 5, 10)
+
+print(x)
+
+# If the value is not found, the find() method returns -1, but the index() method will raise an exception:
+txt = "Hello, welcome to my world."
+
+print(txt.find("q"))
+print(txt.index("q"))
