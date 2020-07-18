33c5631c4dcd308d50f8fdc0a5af370a32627430
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-07-18 22:01:43 +0530

    Capatalize.py

---
+++ Python_String_Methods/Capatalize.py
@@ -0,0 +1,17 @@
+# Note: All string methods returns new values. They do not change the original string.
+# Converts the first character to upper case
+# Syntax :- string.capitalize()
+
+# Upper case the first letter in this sentence:
+txt = "hello, and welcome to my world."
+
+x = txt.capitalize()
+
+print (x)
+
+# See what happens if the first character is a number:
+txt = "21 is my age."
+
+x = txt.capitalize()
+
+print (x)
