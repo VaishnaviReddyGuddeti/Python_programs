32131ea6e43e361a6ee3d577ea15ccb3daccf394
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-07-18 22:08:12 +0530

    Casefold.py

---
+++ Python_String_Methods/Casefold.py
@@ -0,0 +1,9 @@
+#casefold() - Converts string into lower case
+# Syntax - string.casefold()
+
+# Make the string lower case:
+txt = "Hello, And Welcome To My World!"
+
+x = txt.casefold()
+
+print(x)
