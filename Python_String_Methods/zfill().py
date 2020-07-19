d854d1555552b44f0f748fd16939f00e5692e898
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-07-19 16:05:47 +0530

    zfill().py

---
+++ Python_String_Methods/zfill().py
@@ -0,0 +1,19 @@
+# zfill() - Fills the string with a specified number of 0 values at the beginning
+
+# Fill the string with zeros until it is 10 characters long:
+
+txt = "50"
+
+x = txt.zfill(10)
+
+print(x)
+
+# Fill the strings with zeros until they are 10 characters long:
+
+a = "hello"
+b = "welcome to the jungle"
+c = "10.000"
+
+print(a.zfill(10))
+print(b.zfill(10))
+print(c.zfill(10))
