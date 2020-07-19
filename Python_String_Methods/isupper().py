39f4206e4790cff05369d564b4f40e84092bbff5
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-07-19 14:53:17 +0530

    isupper().py

---
+++ Python_String_Methods/isupper().py
@@ -0,0 +1,19 @@
+# isupper() - Returns True if all characters in the string are upper case
+
+# Check if all the characters in the text are in upper case:
+
+txt = "THIS IS NOW!"
+
+x = txt.isupper()
+
+print(x)
+
+# Check if all the characters in the texts are in upper case:
+
+a = "Hello World!"
+b = "hello 123"
+c = "MY NAME IS SUSHANTH"
+
+print(a.isupper())
+print(b.isupper())
+print(c.isupper())
