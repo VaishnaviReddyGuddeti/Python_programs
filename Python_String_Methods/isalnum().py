1c62e659671eec9658ec4085e4d843a45b67acdb
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-07-19 14:01:46 +0530

    isalnum().py

---
+++ Python_String_Methods/isalnum().py
@@ -0,0 +1,14 @@
+# isalmun() - Returns True if all characters in the string are alphanumeric
+# Check if all the characters in the text are alphanumeric:
+txt = "Company12"
+
+x = txt.isalnum()
+
+print(x)
+
+# Check if all the characters in the text is alphanumeric:
+txt = "Company 12"
+
+x = txt.isalnum()
+
+print(x)
