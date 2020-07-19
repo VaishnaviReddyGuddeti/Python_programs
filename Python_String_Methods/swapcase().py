ce12ac90c0e8891dc6964919013d56c9f3c1cc1d
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-07-19 15:59:47 +0530

    swapcase().py

---
+++ Python_String_Methods/swapcase().py
@@ -0,0 +1,9 @@
+# swapcase() - Swaps cases, lower case becomes upper case and vice versa
+
+# Make the lower case letters upper case and the upper case letters lower case:
+
+txt = "Hello My Name Is ANURAG"
+
+x = txt.swapcase()
+
+print(x)
