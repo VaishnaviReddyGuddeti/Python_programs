a3ea078255cfe59070f7c539a0972c08b817ca66
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-07-19 14:42:57 +0530

    isprintable().py

---
+++ Python_String_Methods/isprintable().py
@@ -0,0 +1,17 @@
+# isprintable() - Returns True if all characters in the string are printable
+
+# Check if all the characters in the text are printable:
+
+txt = "Hello! Are you #1?"
+
+x = txt.isprintable()
+
+print(x)
+
+# Check if all the characters in the text are printable:
+
+txt = "Hello!\nAre you #1?"
+
+x = txt.isprintable()
+
+print(x)
