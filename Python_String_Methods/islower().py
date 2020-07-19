a16a68e881f902b1948525d8fb2ec4e20a055668
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-07-19 14:37:03 +0530

    islower().py

---
+++ Python_String_Methods/islower().py
@@ -0,0 +1,17 @@
+# islower() - Returns True if all characters in the string are lower case
+
+# Check if all the characters in the text are in lower case:
+txt = "hello world!"
+
+x = txt.islower()
+
+print(x)
+
+# Check if all the characters in the texts are in lower case:
+a = "Hello world!"
+b = "hello 123"
+c = "mynameisPeter"
+
+print(a.islower())
+print(b.islower())
+print(c.islower())
