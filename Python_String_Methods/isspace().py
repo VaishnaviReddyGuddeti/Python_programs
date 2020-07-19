0a3cfe82c4d9fe8c3cb9090c6ef8f92f322a4695
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-07-19 14:45:29 +0530

    isspace().py

---
+++ Python_String_Methods/isspace().py
@@ -0,0 +1,17 @@
+# isspace() - Returns True if all characters in the string are whitespaces
+
+# Check if all the characters in the text are whitespaces:
+
+txt = "   "
+
+x = txt.isspace()
+
+print(x)
+
+# Check if all the characters in the text are whitespaces:
+
+txt = "   s   "
+
+x = txt.isspace()
+
+print(x)
