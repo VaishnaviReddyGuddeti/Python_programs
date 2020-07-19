1dea4937be9577a3d5296b0cd650987bc1356674
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-07-19 16:02:01 +0530

    title().py

---
+++ Python_String_Methods/title().py
@@ -0,0 +1,25 @@
+# title() - Converts the first character of each word to upper case
+
+# Make the first letter in each word upper case:
+
+txt = "Welcome to my world"
+
+x = txt.title()
+
+print(x)
+
+# Make the first letter in each word upper case:
+
+txt = "Welcome to my 2nd world"
+
+x = txt.title()
+
+print(x)
+
+# Note that the first letter after a non-alphabet letter is converted into a upper case letter:
+
+txt = "hello b2b2b2 and 3g3g3g"
+
+x = txt.title()
+
+print(x)
