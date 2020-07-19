14a1734153d3fa55b0b15df8b7eadcd687dc9815
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-07-19 14:34:43 +0530

    isidentifier().py

---
+++ Python_String_Methods/isidentifier().py
@@ -0,0 +1,17 @@
+# isidentifier() - Returns True if the string is an identifier
+txt = "Demo"
+
+x = txt.isidentifier()
+
+print(x)
+
+# Check if the strings are valid identifiers:
+a = "MyFolder"
+b = "Demo002"
+c = "2bring"
+d = "my demo"
+
+print(a.isidentifier())
+print(b.isidentifier())
+print(c.isidentifier())
+print(d.isidentifier())
