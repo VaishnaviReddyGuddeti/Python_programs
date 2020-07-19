04a7652381e9c807c5fa65f1f734ccbac0a13970
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-07-19 14:05:34 +0530

    isalpha().py

---
+++ Python_String_Methods/isalpha().py
@@ -0,0 +1,14 @@
+# isalpha() - Returns True if all characters in the string are in the alphabet
+# Check if all the characters in the text are letters:
+txt = "CompanyX"
+
+x = txt.isalpha()
+
+print(x)
+
+# Check if all the characters in the text is alphabetic:
+txt = "Company10"
+
+x = txt.isalpha()
+
+print(x)
