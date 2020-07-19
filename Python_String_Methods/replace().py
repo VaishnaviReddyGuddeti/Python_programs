f87cfd7e25788b9f1ae03bf7386f38f688638172
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-07-19 15:23:05 +0530

    replace().py

---
+++ Python_String_Methods/replace().py
@@ -0,0 +1,17 @@
+# replace() - Returns a string where a specified value is replaced with a specified value
+
+# Replace the word "bananas":
+
+txt = "I like toys"
+
+x = txt.replace("boll", "cars")
+
+print(x)
+
+# Replace all occurrence of the word "one":
+
+txt = "one one was a race horse, two two was one too."
+
+x = txt.replace("one", "three")
+
+print(x)
