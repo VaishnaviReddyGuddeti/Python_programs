56baf8d3036ecd417bec65f3be8882d5750d8645
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-07-21 18:15:54 +0530

    ExceptionHandling.py

---
+++ Python_Try_Except/ExceptionHandling.py
@@ -0,0 +1,10 @@
+# When an error occurs, or exception as we call it, Python will normally stop and generate an error message.
+# These exceptions can be handled using the try statement:
+
+# The try block will generate an exception, because x is not defined:
+#The try block will generate an error, because x is not defined:
+
+try:
+  print(x)
+except:
+  print("An exception occurred")
