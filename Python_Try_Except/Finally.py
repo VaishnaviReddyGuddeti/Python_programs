375f03c03ba304819dd0f74d0ecca6c04f1cb31d
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-07-21 21:53:40 +0530

    Finally.py

--- Python_Try_Except/Finally.py
+++ Python_Try_Except/Finally.py
@@ -8,3 +8,17 @@ except:
   print("Something went wrong")
 finally:
   print("The 'try except' is finished")
+
+# Try to open and write to a file that is not writable:
+
+#The try block will raise an error when trying to write to a read-only file:
+
+try:
+  f = open("demofile.txt")
+  f.write("Lorum Ipsum")
+except:
+  print("Something went wrong when writing to the file")
+finally:
+  f.close()
+
+#The program can continue, without leaving the file object open
