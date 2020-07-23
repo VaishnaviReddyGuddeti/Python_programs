2e9dcf5e2af7da5e6fa8e8552578fc9984b723b9
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-07-23 19:23:41 +0530

    Valuesaretrue.py

---
+++ Python_Boolean/Valuesaretrue.py
@@ -0,0 +1,6 @@
+# Any value is evaluated to True if it has some sort of content.
+
+# The following will return True
+print(bool("abc"))
+print(bool(123))
+print(bool(["apple", "cherry", "banana"]))
