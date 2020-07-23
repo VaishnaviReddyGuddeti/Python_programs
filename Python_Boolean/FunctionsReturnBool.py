9b0d6cd8e2ab24be959417521c86021851b69892
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-07-23 20:36:51 +0530

    FunctionsReturnBool.py

---
+++ Python_Boolean/FunctionsReturnBool.py
@@ -0,0 +1,23 @@
+# You can create functions that returns a Boolean Value:
+
+#Print the answer of a function:
+
+def myFunction() :
+  return True
+
+print(myFunction())
+
+# Print "YES!" if the function returns True, otherwise print "NO!":
+
+def myFunction() :
+  return True
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")
+
+# Check if an object is an integer or not:
+
+x = 200
+print(isinstance(x, int))
