13cff9723ad634f8bf011752012e9247dd2fcb17
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-07-24 14:33:48 +0530

    Format_map().py

---
+++ Python_String_Methods/Format_map().py
@@ -0,0 +1,6 @@
+# Formats specified values in a string
+
+# input stored in variable a
+a = {'x':'Viraj' , 'y':'Reddy'}
+# use of format_map() function
+print("{x}'s last name is {y}".format_map(a))
