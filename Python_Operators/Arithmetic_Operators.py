e1cdf7e01e5d1b5bd02740b7c600fa02ee785fbc
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-07-26 12:40:50 +0530

    Arithmetic_Operators.py

--- Python_Operators/Arithmetic_Operators.py
+++ Python_Operators/Arithmetic_Operators.py
@@ -1,1 +1,8 @@
 # Arithmetic operators are used with numeric values to perform common mathematical operations
+Operator	     Name	  Example
+-	          Subtraction	x - y
+*        	Multiplication	x * y
+/            	Division	x / y
+%	              Modulus	x % y
+**	       Exponentiation	x ** y
+//	        Floor division  x // y
