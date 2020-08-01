9f4feb585d0e2d9f32085fe222ef1d2604292e62
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-01 18:19:03 +0530

    Boolean.py

--- Python_Boolean/Boolean.py
+++ Python_Boolean/Boolean.py
@@ -1,5 +1,4 @@
 # You can evaluate any expression in Python, and get one of two answers, True or False.
-
 # When you compare two values, the expression is evaluated and Python returns the Boolean answer:
 print(10 > 9)
 print(10 == 9)
@@ -8,7 +7,6 @@ print(10 < 9)
 # Print a message based on whether the condition is True or False:
 a = 99
 b = 33
-
 if b > a:
   print("b is greater than a")
 else:
