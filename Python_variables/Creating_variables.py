9c8d361eb01b51c24b610880d90f934cc414ad8b
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-01 18:33:33 +0530

    Creating_variables.py

--- Python_variables/Creating_variables.py
+++ Python_variables/Creating_variables.py
@@ -1,19 +1,17 @@
-# Variables are containers for storing data values.
-
-# Unlike other programming languages, Python has no command for declaring a variable.
-
-# A variable is created the moment you first assign a value to it.
+# Variables are containers for storing data values
+# Unlike other programming languages, Python has no command for declaring a variable
+# A variable is created the moment you first assign a value to it
 x = 5
 y = "John"
 print(x)
 print(y)
-# Variables do not need to be declared with any particular type and can even change type after they have been set.
+# Variables do not need to be declared with any particular type and can even change type after they have been set
 x = 4
 x = "Sally"
 print(x)
-# String variables can be declared either by using single or double quotes:
+# String variables can be declared either by using single or double quotes
 x = "John"
 print(x)
-#double quotes are the same as single quotes:
+#double quotes are the same as single quotes
 x = 'John'
 print(x)
