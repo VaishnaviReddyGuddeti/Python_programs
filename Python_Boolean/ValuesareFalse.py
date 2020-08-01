1df1fb021985a88fe09fa079fe1069494a17450a
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-01 18:22:39 +0530

    ValuesareFalse.py

--- Python_Boolean/ValuesareFalse.py
+++ Python_Boolean/ValuesareFalse.py
@@ -1,5 +1,4 @@
 #There are not many values that evaluates to False, except empty values, such as (), [], {}, "", the number 0, and the value None. And of course the value False evaluates to False.
-
 # The following will return False:
 print(bool(False))
 print(bool(None))
@@ -8,12 +7,9 @@ print(bool(""))
 print(bool(()))
 print(bool([]))
 print(bool({}))
-
 # if you have an object that is made from a class with a __len__ function that returns 0 or False:
-
 class myclass():
   def __len__(self):
     return 0
-
 myobj = myclass()
 print(bool(myobj))
