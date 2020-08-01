8d616f6d8d4045839d47a9d11bd7ed6b336a6190
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-01 11:29:22 +0530

    Arithmetic_Operators.py

--- Python_Operators/Arithmetic_Operators.py
+++ Python_Operators/Arithmetic_Operators.py
@@ -1,8 +1,20 @@
-# Arithmetic operators are used with numeric values to perform common mathematical operations
-Operator	     Name	  Example
--	          Subtraction	x - y
-*        	Multiplication	x * y
-/            	Division	x / y
-%	              Modulus	x % y
-**	       Exponentiation	x ** y
-//	        Floor division  x // y
+a = "Operators are used to perform operations on variables and values.\\\
+Python divides the operators in the following groups:\\\
+\n "+" \\\
+\n Assignment operators\\\
+\n Comparison operators\\\
+\n Logical operators\\\
+\n Identity operators\\\
+\n Membership operators\\\
+\n Bitwise operators "
+print(a)
+# if condition with arithmetic operator.
+# x = (5 > 3 or 5 > 10)
+print("Enter Value")
+x = input()
+x = int(x)
+# if 5 > 3 or 5 > 10:
+if (x > 3 | x < 10):
+    print("At least one of the statements are True")
+else:
+    print("None of the statements are True")
