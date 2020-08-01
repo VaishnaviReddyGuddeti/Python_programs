ae7ab9eb37df286ab1f629eec4a6a25cc63d08fc
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-01 18:28:23 +0530

    Creating_a_Comment.py

--- Python_comments/Creating_a_Comment.py
+++ Python_comments/Creating_a_Comment.py
@@ -1,13 +1,11 @@
-# Comments can be used to explain Python code.
-
-# Comments can be used to make the code more readable.
-
-# Comments can be used to prevent execution when testing code.
-# Comments starts with a #, and Python will ignore them:
-#This is a comment.
+# Comments can be used to explain Python code
+# Comments can be used to make the code more readable
+# Comments can be used to prevent execution when testing code
+# Comments starts with a #, and Python will ignore them
+#This is a comment
 print("Hello, World!")
-# Comments can be placed at the end of a line, and Python will ignore the rest of the line:
-print("Hello, World!") #This is a comment.
-# Comments does not have to be text to explain the code, it can also be used to prevent Python from executing code:
+# Comments can be placed at the end of a line, and Python will ignore the rest of the line
+print("Hello, World!") #This is a comment
+# Comments does not have to be text to explain the code, it can also be used to prevent Python from executing code
 #print("Hello, World!")
 print("Cheers, Mate!")
