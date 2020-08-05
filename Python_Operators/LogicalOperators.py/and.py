584fce0ddc77677682f18f9520a242c2b90f1016
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-05 18:01:32 +0530

    and.py

---
+++ Python_Operators/LogicalOperators.py/and.py
@@ -0,0 +1,9 @@
+x = 5
+
+print(x > 3 and x < 10)
+
+# returns True because 5 is greater than 3 AND 5 is less than 10
+
+x = 3
+print(x > 4 and x < 1)
+# returns False because 3 is not greater than 4 AND 3 is not less than 1
