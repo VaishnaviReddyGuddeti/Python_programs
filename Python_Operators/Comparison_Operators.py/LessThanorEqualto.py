c7695618a3aca58ae3fbdc5bf8d4fd0f2ec7cf69
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-05 17:54:20 +0530

    LessThanorEqualto.py

---
+++ Python_Operators/Comparison_Operators.py/LessThanorEqualto.py
@@ -0,0 +1,14 @@
+x = 5
+y = 3
+
+print(x <= y)
+# returns False because 5 is neither less than or equal to 3
+
+x = 4
+y = 7
+print(x <= y)
+# returns True because 4 is less than or equal to 7
+x = 9
+y = 9
+print(x <= y)
+# returns True because both the values are equal
