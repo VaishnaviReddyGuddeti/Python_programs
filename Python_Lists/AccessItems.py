7171d80636d31080f2a663b1b4b5848e951814cc
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-06 20:30:31 +0530

    AccessItems.py

---
+++ Python_Lists/AccessItems.py
@@ -0,0 +1,7 @@
+#  You access the list items by referring to the index number:
+# Print the second item of the list:
+
+thislist = ["apple", "ice-cream", "cherry"]
+print(thislist[1])
+print(thislist[0])
+print(thislist[2])
