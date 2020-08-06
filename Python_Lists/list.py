75bd46032879ed971fbb073de3b75b487f5511ba
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-06 20:25:07 +0530

    list.py

---
+++ Python_Lists/list.py
@@ -0,0 +1,5 @@
+# A list is a collection which is ordered and changeable. In Python lists are written with square brackets.
+#Create a List:
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
