281935f1d09006410c34c33fc647d55050fe48f9
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-07 19:26:52 +0530

    NegativeIndexing.py

---
+++ Python_Lists/NegativeIndexing.py
@@ -0,0 +1,7 @@
+#Negative indexing means beginning from the end, -1 refers to the last item, -2 refers to the second last item
+# print the last item of the list
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist[-1])
+print(thislist[-2])
+print(thislist[-3])
