c4d99b3c0d2b08843e5deb86e3ac864cdab0d414
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-07 19:31:33 +0530

    RangeofIndexes.py

---
+++ Python_Lists/RangeofIndexes.py
@@ -0,0 +1,9 @@
+# You can specify a range of indexes by specifying where to start and where to end the range
+# Return the third, fourth, and fifth item:
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
+
+#This will return the items from position 2 to 5.
+
+#Remember that the first item is position 0,
+#and note that the item in position 5 is NOT included
