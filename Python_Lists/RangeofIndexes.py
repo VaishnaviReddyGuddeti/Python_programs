ef231d314bee23987a777e00e684abd5f53e18bc
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-08 12:15:18 +0530

    RangeofIndexes.py

--- Python_Lists/RangeofIndexes.py
+++ Python_Lists/RangeofIndexes.py
@@ -14,3 +14,8 @@ thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
 print(thislist[:4])
 #Remember that index 0 is the first item, and index 4 is the fifth item
 #Remember that the item in index 4 is NOT included
+# By leaving out the end value, the range will go on to the end of the list:
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:])
+#This will return the items from index 2 to the end.
+#Remember that index 0 is the first item, and index 2 is the third
