9370a8e07cf4575c48fa1c34cff0616f4d07f4e5
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-12 19:47:45 +0530

    index().py

---
+++ Python_Lists/ListMethod.py/index().py
@@ -0,0 +1,11 @@
+# Returns the index of the first element with the specified value
+# What is the position of the value "Bihar":
+
+states = ['Bihar', 'Uttarakhand', 'Rajasthan']
+x = states.index("Bihar")
+print(x)
+
+# Whatis the position of "Mg Hector"
+cars = ['Maserati Quattroporte' , 'BMW' , 'MG Hector']
+x = cars.index("MG Hector")
+print(x)
