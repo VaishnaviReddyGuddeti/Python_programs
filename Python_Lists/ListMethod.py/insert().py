9bfe3257534fa34cf0132e11cd2240874ff5eed4
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-12 20:10:25 +0530

    insert().py

---
+++ Python_Lists/ListMethod.py/insert().py
@@ -0,0 +1,6 @@
+# Adds an element at the specified position
+# Insert the value "IBM" as the second element of the fruit list:
+
+companies = ['Google', 'Apple', 'IBM']
+companies.insert(1, "TechM")
+print(companies)
