0e4cd4cc5da87745284f4c472c5e8aecc5083a8f
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-12 18:41:11 +0530

    append().py

---
+++ Python_Lists/ListMethod.py/append().py
@@ -0,0 +1,21 @@
+# Adds an element at the end of the list
+fruits = ["apple", "banana", "cherry"]
+fruits.append("mango")
+print(fruits)
+
+# Add a list to a list:
+
+a = ["apple", "mango", "orange"]
+b = ["Ford", "BMW", "Volvo"]
+a.append(b)
+print(a)
+
+# Add two lists to a lists:
+
+a = ["Royal Enfield", "Harley Davidson", "BMW K 1300 R"]
+b = ["Maserati Quattroporte" , "BMW","MG Hector"]
+c = ["GULAB JAMUN" , "JALEBI" , "RAS MALAI"]
+# a.append(b,c)
+a.append(b)
+a.append(c)
+print(a)
