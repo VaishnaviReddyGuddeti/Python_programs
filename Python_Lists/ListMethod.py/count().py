4ead5f9aeda211ea2bae5a3b58b2e6820f80500a
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-12 19:20:54 +0530

    count().py

---
+++ Python_Lists/ListMethod.py/count().py
@@ -0,0 +1,9 @@
+# Returns the number of elements with the specified value
+# Return the number of times the value "jalebi " appears in the sweets list:
+sweets = ["GULAB JAMUN" , "JALEBI" , "RAS MALAI"]
+x = sweets.count("JALEBI")
+print(x)
+
+cars = ["Maserati Quattroporte" , "BMW","MG Hector"]
+x = cars.count("Maserati Quattroporte")
+print(x)
