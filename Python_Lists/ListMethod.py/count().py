d5b770749a0d91c4d6a69ece9be2c267c125eaee
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-12 19:32:39 +0530

    clear()method.py

--- Python_Lists/ListMethod.py/count().py
+++ Python_Lists/ListMethod.py/count().py
@@ -7,3 +7,8 @@ print(x)
 cars = ["Maserati Quattroporte" , "BMW","MG Hector"]
 x = cars.count("Maserati Quattroporte")
 print(x)
+
+# Return the number of times the value 9 appears int the list:
+points = [1, 4, 2, 9, 7, 8, 9, 3, 1]
+x = points.count(9)
+print(x)
