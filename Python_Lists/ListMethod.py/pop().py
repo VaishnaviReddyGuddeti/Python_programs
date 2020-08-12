30cd650bc81ca0420e1b5ec18e5be4a033c0154a
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-12 20:13:18 +0530

    pop().py

---
+++ Python_Lists/ListMethod.py/pop().py
@@ -0,0 +1,12 @@
+# Removes the element at the specified position
+# Remove the second element of the fruit list:
+
+fruits = ['apple', 'banana', 'cherry']
+fruits.pop(1)
+print(fruits)
+
+# Return the removed element:
+
+fruits = ['apple', 'banana', 'cherry']
+x = fruits.pop(1)
+print(x)
