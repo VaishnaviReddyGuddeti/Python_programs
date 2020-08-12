625ce8b1a0852349747bf4a78cd15a8de92e84ba
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-12 20:32:52 +0530

    sort().py

---
+++ Python_Lists/ListMethod.py/sort().py
@@ -0,0 +1,12 @@
+# Sorts the list
+# Sort the list alphabetically
+
+cars = ["Maserati Quattroporte" , "BMW","MG Hector"]
+cars.sort()
+print(cars)
+
+# Sort the list descending
+
+cars = ['Ford', 'BMW', 'Volvo']
+cars.sort(reverse=True)
+print(cars)
