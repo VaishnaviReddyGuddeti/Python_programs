80c11a0510afb5e8bc9feab23516354c768316fe
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-13 12:16:22 +0530

    JoinTwoTuples.py

---
+++ Python_Tuples/JoinTwoTuples.py
@@ -0,0 +1,13 @@
+# To join two or more tuples you can use the + operator:
+# Join two tuples:
+
+tuple1 = ("a", "b" , "c")
+tuple2 = (1, 2, 3)
+
+tuple3 = tuple1 + tuple2
+print(tuple3)
+
+tuple1 = ("Royal Enfield", "Harley Davidson", "BMW K 1300 R")
+tuple2 = ("Maserati Quattroporte" , "BMW","MG Hector")
+tuple3 = tuple1 + tuple2
+print(tuple3)
