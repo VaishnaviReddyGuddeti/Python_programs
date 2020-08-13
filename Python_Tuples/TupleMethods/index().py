bf74036cf0c09e77e69aaf12156b311154223b7c
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-13 12:29:13 +0530

    index().py

---
+++ Python_Tuples/TupleMethods/index().py
@@ -0,0 +1,8 @@
+# Searches the tuple for a specified value and returns the position of where it was found
+# Search for the first occurrence of the value 8, and return its position:
+
+thistuple = (1, 3, 7, 8, 7, 5, 4, 6, 8, 5)
+
+x = thistuple.index(8)
+
+print(x)
