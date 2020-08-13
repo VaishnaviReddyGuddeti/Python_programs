1a3025e11e823ee665c64d3d22bd0e4fcb80a344
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-13 21:31:45 +0530

    AccessingItems.py

--- Python_Dictionary/AccessingItems.py
+++ Python_Dictionary/AccessingItems.py
@@ -7,3 +7,13 @@ thisdict =	{
 }
 x = thisdict["model"]
 print(x)
+
+# There is also a method called get() that will give you the same result:
+# Get the value of the "model" key:
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+x = thisdict.get("model")
+print(x)
