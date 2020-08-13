3858a23f5b08fb33b88da89411338d50e05085ac
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-13 12:17:59 +0530

    Tuple()Constructor.py

---
+++ Python_Tuples/Tuple()Constructor.py
@@ -0,0 +1,5 @@
+# It is also possible to use the tuple() constructor to make a tuple.
+# Using the tuple() method to make a tuple:
+
+thistuple = tuple(("apple", "banana", "cherry")) # note the double round-brackets
+print(thistuple)
