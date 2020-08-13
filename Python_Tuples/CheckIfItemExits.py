2f2136d837082145a35f4f5acf3822bec1d71660
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-13 12:01:46 +0530

    CheckIfItemExits.py

---
+++ Python_Tuples/CheckIfItemExits.py
@@ -0,0 +1,6 @@
+# To determine if a specified item is present in a tuple use the in keyword:
+# Check if "apple" is present in the tuple:
+
+thistuple = ("apple", "banana", "cherry")
+if "apple" in thistuple:
+  print("Yes, 'apple' is in the fruits tuple")
