44e74b30dd6b375ea8da22e105e5e2c10f94aa72
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-13 12:27:28 +0530

    count().py

---
+++ Python_Tuples/TupleMethods/count().py
@@ -0,0 +1,13 @@
+# Python has two built-in methods that you can use on tuples.
+# Returns the number of times a specified value occurs in a tuple
+
+# Return the number of times the value 5 appears in the tuple:
+
+thistuple = (1, 3, 7, 8, 7, 5, 4, 6, 8, 5)
+x = thistuple.count(5)
+print(x)
+
+# Return the number of times the "BMW K 1300 R" appears in the tuple
+thistuple = ("Royal Enfield" , "Harley Davidson" , "BMW K 1300 R" , "Maserati Quattroporte" , "BMW" , "MG Hector" , "BMW K 1300 R")
+x = thistuple.count(BMW K 1300 R)
+print(x)
