1b8bd13f5ff334b35df1c6b8e13136e6c99c63e0
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-13 21:50:09 +0530

    DictionaryLength.py

---
+++ Python_Dictionary/DictionaryLength.py
@@ -0,0 +1,9 @@
+# To determine how many items (key-value pairs) a dictionary has, use the len() function.
+# Print the number of items in the dictionary:
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+print(len(thisdict))
