dd944f74f5ee44dd856dcf2214a34b006744b3ea
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-13 22:19:14 +0530

    Thedict()Constructor.py

---
+++ Python_Dictionary/Thedict()Constructor.py
@@ -0,0 +1,5 @@
+# It is also possible to use the dict() constructor to make a new dictionary:
+thisdict = dict(brand="Ford", model="Mustang", year=1964)
+# note that keywords are not string literals
+# note the use of equals rather than colon for the assignment
+print(thisdict)
