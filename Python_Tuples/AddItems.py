0e4b6adc2108196f0b90c584eec5f75bb9f2016a
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-13 12:06:50 +0530

    AddItems.py

---
+++ Python_Tuples/AddItems.py
@@ -0,0 +1,6 @@
+# Once a tuple is created, you cannot add items to it. Tuples are unchangeable.
+# You cannot add items to a tuple:
+
+thistuple = ("apple", "banana", "cherry")
+thistuple[3] = "orange" # This will raise an error
+print(thistuple)
