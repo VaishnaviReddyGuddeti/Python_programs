8c93d348ee08df5f72b7dc2b51ad8c4d202ff434
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-13 11:47:51 +0530

    RangeOfIndexes.py

---
+++ Python_Tuples/RangeOfIndexes.py
@@ -0,0 +1,11 @@
+# You can specify a range of indexes by specifying where to start and where to end the range.
+# When specifying a range, the return value will be a new tuple with the specified items.
+
+#Return the third, fourth, and fifth item:
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:5])
+
+#This will return the items from position 2 to 5.
+
+#Remember that the first item is position 0,
+#and note that the item in position 5 is NOT included
