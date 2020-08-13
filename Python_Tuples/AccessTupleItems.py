bd45c3b6a888e4511d6f17f8d96b5bd31bd72fd7
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-13 11:09:40 +0530

    AccessTupleItems.py

---
+++ Python_Tuples/AccessTupleItems.py
@@ -0,0 +1,5 @@
+# You can access tuple items by referring to the index number, inside square brackets:
+# Print the second item in the tuple:
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[1])
