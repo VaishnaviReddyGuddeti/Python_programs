a9ef38120d590c34e243ae22f11cfdcc4bb1a7f7
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-14 16:20:50 +0530

    LoopingThroughanIterator.py

---
+++ Python_Iterators/LoopingThroughanIterator.py
@@ -0,0 +1,15 @@
+# We can also use a for loop to iterate through an iterable object:
+
+# Iterate the values of a tuple:
+
+mytuple = ("apple", "banana", "cherry")
+
+for x in mytuple:
+  print(x)
+
+ # Iterate the characters of a string:
+
+mystr = "arush"
+
+for x in mystr:
+  print(x)
