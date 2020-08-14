60bfeeb62db6e83b270e65adc10febb7169c4982
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-14 16:17:19 +0530

    IteratorvsIterable.py

---
+++ Python_Iterators/IteratorvsIterable.py
@@ -0,0 +1,23 @@
+# All these objects have a iter() method which is used to get an iterator:
+
+#Return an iterator from a tuple, and print each value:
+
+mytuple = ("apple", "banana", "cherry")
+myit = iter(mytuple)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+
+# Even strings are iterable objects, and can return an iterator:
+# Strings are also iterable objects, containing a sequence of characters:
+
+mystr = "banana"
+myit = iter(mystr)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
