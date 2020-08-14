1bc0b4ec69d61e335be6cf0606bf6f3ddbb1481c
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-14 14:19:26 +0530

    update().py

---
+++ Python_Dictionary/Dictionary Methods/update().py
@@ -0,0 +1,13 @@
+# Updates the dictionary with the specified key-value pairs
+
+# Insert an item to the dictionary:
+
+car = {
+  "brand": "BMW",
+  "model": "X5",
+  "year": 2020
+}
+
+car.update({"color": "Black"})
+
+print(car)
