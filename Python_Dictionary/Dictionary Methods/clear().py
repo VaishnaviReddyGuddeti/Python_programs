4b4860912298aee25fab0999d056c8aa370c7568
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-14 11:29:38 +0530

    clear().py

---
+++ Python_Dictionary/Dictionary Methods/clear().py
@@ -0,0 +1,12 @@
+# Removes all the elements from the dictionary
+# Remove all elements from the car list:
+
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+car.clear()
+
+print(car)
