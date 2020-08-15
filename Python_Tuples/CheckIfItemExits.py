f7b7ea5246ce10812f1c698e5ff3c981ea133200
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-15 16:32:40 +0530

    CheckIfItemExits.py

--- Python_Tuples/CheckIfItemExits.py
+++ Python_Tuples/CheckIfItemExits.py
@@ -4,3 +4,5 @@
 thistuple = ("apple", "banana", "cherry")
 if "apple" in thistuple:
   print("Yes, 'apple' is in the fruits tuple")
+if "kiwi" not in thistuple:
+    print("No, 'kiwi' is not in the fruits tuple")
