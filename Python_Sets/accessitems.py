13ebd08e1998de106381e1a79eb1102d5a7e9880
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-16 10:32:04 +0530

    accessitems.py

--- Python_Sets/accessitems.py
+++ Python_Sets/accessitems.py
@@ -7,3 +7,12 @@ thisset = {"apple", "banana", "cherry"}
 
 for x in thisset:
   print(x)
+
+# Check if "banana" is present in the set:
+
+thisset = {"apple", "banana", "cherry"}
+
+print("banana" in thisset)
+
+thisset = {"apple", "banana", "cherry"}
+print("kiwi" in thisset)
