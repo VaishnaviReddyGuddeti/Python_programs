a2e43db3ff2d51be8f2921da4d5b58a75b4d09b6
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-16 13:49:36 +0530

    theset()constructor.py

---
+++ Python_Sets/theset()constructor.py
@@ -0,0 +1,6 @@
+# It is also possible to use the set() constructor to make a set.
+
+# sing the set() constructor to make a set:
+
+thisset = set(("apple", "banana", "cherry")) # note the double round-brackets
+print(thisset)
