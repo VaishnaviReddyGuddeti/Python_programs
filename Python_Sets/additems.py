75e206b7a21fe9cef6fbd2fe498a26da555512c7
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-16 10:36:22 +0530

    additems.py

---
+++ Python_Sets/additems.py
@@ -0,0 +1,15 @@
+# To add one item to a set use the add() method.
+
+# To add more than one item to a set use the update() method.
+
+# Add an item to a set, using the add() method:
+
+thisset = {"apple", "banana", "cherry"}
+thisset.add("orange")
+print(thisset)
+
+# Add multiple items to a set, using the update() method:
+
+thisset = {"BMW", "Maserati", "MG Hector"}
+thisset.update(["Black", "Blue", "White"])
+print(thisset)
