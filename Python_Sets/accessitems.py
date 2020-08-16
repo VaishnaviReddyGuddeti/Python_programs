15f4ccfd6b88dada1d660ce4305e53e6c293a2cc
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-16 10:30:04 +0530

    accessitems.py

---
+++ Python_Sets/accessitems.py
@@ -0,0 +1,9 @@
+# You cannot access items in a set by referring to an index, since sets are unordered the items has no index.
+
+# But you can loop through the set items using a for loop, or ask if a specified value is present in a set, by using the in keyword.
+# Loop through the set, and print the values:
+
+thisset = {"apple", "banana", "cherry"}
+
+for x in thisset:
+  print(x)
