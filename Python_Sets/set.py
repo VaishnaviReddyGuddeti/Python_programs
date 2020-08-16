c9bae814d7d084168e634543171306c053d1434b
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-16 10:28:17 +0530

    set.py

---
+++ Python_Sets/set.py
@@ -0,0 +1,6 @@
+# A set is a collection which is unordered and unindexed. In Python sets are written with curly brackets.
+
+# Create a Set:
+
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
