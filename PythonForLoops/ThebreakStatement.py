804c9b9d75b06ac851db10aaef4493f5d628d36b
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-18 23:26:45 +0530

    ThebreakStatement.py

--- PythonForLoops/ThebreakStatement.py
+++ PythonForLoops/ThebreakStatement.py
@@ -7,3 +7,10 @@ for x in cars:
   print(x)
   if x == "BMW":
     break
+# Exit the loop when x is "BMW", but this time the break comes before the print:
+
+cars = ["Maserati", "BMW", "MG Hector"]
+for x in cars:
+  if x == "BMW":
+    break
+  print(x)
