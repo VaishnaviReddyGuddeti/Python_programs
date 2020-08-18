2f0a271dfa5d0ad07d6aa2626331a4b62ffd13d0
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-18 12:11:44 +0530

    intersection()update.py

---
+++ Python_Sets/SetMethods/intersection()update.py
@@ -0,0 +1,13 @@
+# Returns a set, that is the intersection of two other sets
+# Return a set that contains the items that exist in both set x, and set y:
+
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+z = x.intersection(y)
+print(z)
+
+
+x = {"apple", "banana", "cherry"}
+y = {"kiwi", "mango", "orange"}
+z = x.intersection(y)
+print(z)
