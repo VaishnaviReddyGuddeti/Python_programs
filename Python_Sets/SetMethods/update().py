91341d7f9460b12111aa40e8bddbb97bdc741553
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-18 22:00:15 +0530

    update().py

---
+++ Python_Sets/SetMethods/update().py
@@ -0,0 +1,7 @@
+# Update the set with the union of this set and others
+# Insert the items from set y into set x:
+
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+x.update(y)
+print(x)
