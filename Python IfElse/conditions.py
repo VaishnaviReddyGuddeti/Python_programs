f3c55abcec080b1a17e940b00098ad9fd0ce81cd
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-18 22:10:15 +0530

    conditions.py

--- Python IfElse/conditions.py
+++ Python IfElse/conditions.py
@@ -1,8 +1,14 @@
 # Python supports the usual logical conditions from mathematics:
+# Equals: a == b
+# Not Equals: a != b
+# Less than: a < b
+# Less than or equal to: a <= b
+# Greater than: a > b
+# Greater than or equal to: a >= b
 
-Equals: a == b
-Not Equals: a != b
-Less than: a < b
-Less than or equal to: a <= b
-Greater than: a > b
-Greater than or equal to: a >= b
+
+# If statement:
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
