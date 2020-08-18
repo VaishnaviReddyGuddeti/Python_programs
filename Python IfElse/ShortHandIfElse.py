e9f8cf85365cacd08eb8787d048e5c1eee6f7512
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-18 22:26:24 +0530

    ShortHandIfElse.py

---
+++ Python IfElse/ShortHandIfElse.py
@@ -0,0 +1,12 @@
+# If you have only one statement to execute, one for if, and one for else, you can put it all on the same line:
+# One line if else statement:
+
+a = 2
+b = 330
+print("A") if a > b else print("B")
+
+# One line if else statement, with 3 conditions:
+
+a = 330
+b = 330
+print("A") if a > b else print("=") if a == b else print("B")
