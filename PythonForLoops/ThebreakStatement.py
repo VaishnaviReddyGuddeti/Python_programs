7149c8bc6bb119353c0b799eea6fc0b8946a1798
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-18 23:20:28 +0530

    ThebreakStatement.py

---
+++ PythonForLoops/ThebreakStatement.py
@@ -0,0 +1,9 @@
+# With the break statement we can stop the loop before it has looped through all the items:
+
+# Exit the loop when x is "BMW":
+
+cars = ["Maserati", "BMW", "MG Hector"]
+for x in cars:
+  print(x)
+  if x == "BMW":
+    break
