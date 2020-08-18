b80b42fc4f8c9010d438db4b9908e0f218dea80b
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-18 23:14:27 +0530

    ForLoop.py

---
+++ PythonForLoops/ForLoop.py
@@ -0,0 +1,8 @@
+# A for loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set, or a string)
+# the for loop we can execute a set of statements, once for each item in a list, tuple, set etc.
+
+# Print each car in a car list:
+
+cars = ["Maserati", "BMW", "MG Hector"]
+for x in cars:
+  print(x)
