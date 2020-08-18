0fde8c329028e267a6f810c5d03daad729e7f53d
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-18 21:50:36 +0530

    pop().py

---
+++ Python_Sets/SetMethods/pop().py
@@ -0,0 +1,12 @@
+# Removes an element from the set
+# Remove a random item from the set:
+
+fruits = {"apple", "banana", "cherry"}
+fruits.pop()
+print(fruits)
+
+# Return the removed element:
+
+fruits = {"apple", "banana", "cherry"}
+x = fruits.pop()
+print(x)
