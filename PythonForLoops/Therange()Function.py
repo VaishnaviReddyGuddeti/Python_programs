85a0172e736da400d04c87fc4619be60667de442
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-19 21:02:58 +0530

    Therange()Function.py

---
+++ PythonForLoops/Therange()Function.py
@@ -0,0 +1,6 @@
+# The range() function returns a sequence of numbers, starting from 0 by default, and increments by 1 (by default), and ends at a specified number.
+
+# Using the range() function:
+
+for x in range(6):
+  print(x)
