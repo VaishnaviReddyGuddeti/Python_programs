d32c290d6e3b3b397c33dd4e31e10255d0d4deeb
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-19 22:16:52 +0530

    Arguments.py

---
+++ Python_Functions/Arguments.py
@@ -0,0 +1,8 @@
+# The following example has a function with one argument (fname). When the function is called, we pass along a first name, which is used inside the function to print the full name:
+
+def my_function(fname):
+  print(fname + " Refsnes")
+
+my_function("Email")
+my_function("Name")
+my_function("Abhi")
