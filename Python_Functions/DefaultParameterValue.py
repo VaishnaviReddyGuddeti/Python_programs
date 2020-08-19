ede45e03aa0b795a6a96172f10545f095e81955e
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-19 22:53:58 +0530

    DefaultParameterValue.py

---
+++ Python_Functions/DefaultParameterValue.py
@@ -0,0 +1,12 @@
+# The following example shows how to use a default parameter value.
+
+# If we call the function without argument, it uses the default value:
+
+
+def my_function(country = "India"):
+  print("I am from " + country)
+
+my_function("Uk")
+my_function("Ireland")
+my_function()
+my_function("Brazil")
