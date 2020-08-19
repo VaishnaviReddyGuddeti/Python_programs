7d4e059909fd1b24ff032379a47f3ad50c340e48
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-19 21:08:30 +0530

    Therange()Function.py

--- PythonForLoops/Therange()Function.py
+++ PythonForLoops/Therange()Function.py
@@ -4,3 +4,10 @@
 
 for x in range(6):
   print(x)
+
+# The range() function defaults to 0 as a starting value, however it is possible to specify the starting value by adding a parameter: range(2, 6), which means values from 2 to 6 (but not including 6):
+
+# Using the start parameter:
+
+for x in range(2, 6):
+  print(x)
