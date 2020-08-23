bff584977822250830f0f976ee46484206e84e4d
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-23 20:16:11 +0530

    Arrays.py

---
+++ Python_Arrays/Arrays.py
@@ -0,0 +1,8 @@
+# Array is a data structure which stores values of same data type.
+# Array os used to store multiple values in one single variable.
+
+# Create an array that contains car names
+
+
+cars = ["Maserati" , "BMW" , "MG Hector"]
+print(cars)
