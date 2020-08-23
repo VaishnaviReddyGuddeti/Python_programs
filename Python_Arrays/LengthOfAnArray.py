43c68e10952d2de55dcb1a889387d6aa54176dc7
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-23 22:18:41 +0530

    LengthOfAnArray.py

---
+++ Python_Arrays/LengthOfAnArray.py
@@ -0,0 +1,22 @@
+# Use the len() method to return the length of an array (the number of elements in an array).
+
+# Return the number of elements in the cars array:
+cars = ["Maserati","BMW","MG Hector"]
+x = len(cars)
+print(x)
+
+num = ["1,2,3,4,5,6,7,8,9"]
+x = len(num)
+print(x)
+
+numbers = ["1","2","3","4","5","6","7","8","9"]
+x = len(numbers)
+print(x)
+
+num1 = []
+x = len(num1)
+print(x)
+
+zero = [""]
+x = len(zero)
+print(x)
