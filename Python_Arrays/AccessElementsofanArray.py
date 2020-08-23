be5e3c1aa49a96f74e71692b1d29d56b3f8f4ab6
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-08-23 20:22:13 +0530

    AccessElementsofanArray.py

---
+++ Python_Arrays/AccessElementsofanArray.py
@@ -0,0 +1,10 @@
+# Get the value of the first array item:
+
+cars = ["Maserati", "BMW", "MG Hector"]
+x = cars[0]
+print(x)
+
+# Modify the value of the first array item:
+cars = ["Audi", "BMW", "MG Hector"]
+cars[0] = "Maserati"
+print(cars)
