f3a713934a27cddd307279dbb2e8e7f9f0d4b1fd
Vaishnavi Reddy <vaishnavireddyguddeti@gmail.com>
2020-11-24 20:33:23 +0530

    CreateAClass.py

---
+++ Python_Classes_Objects/CreateAClass.py
@@ -0,0 +1,7 @@
+# Everything in Python is an object, with its properties and methods.
+# A Class is like an object constructor, or a "blueprint" for creating objects.
+# To create a class, use the keyword class:
+# Create a class named MyClass, with a property named x:
+class MyClass:
+  x = 5
+print(MyClass)
