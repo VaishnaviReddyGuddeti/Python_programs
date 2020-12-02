443948e478ae959aa4d3bae2269647bb0d096f25
VaishnaviReddyGuddeti <vaishnavireddyguddeti@gmail.com>
2020-12-02 19:36:05 +0530

    WhyPython.py

--- Python_Introduction/WhyPython.py
+++ Python_Introduction/WhyPython.py
@@ -3,3 +3,4 @@
 3.Python has syntax that allows developers to write programs with fewer lines than some other programming languages.
 4.Python runs on an interpreter system, meaning that code can be executed as soon as it is written. This means that prototyping can be very quick.
 5.Python can be treated in a procedural way, an object-oriented way or a functional way.
+# the latest version of python is (Python 3.9. 0, documentation released on 5 October 2020.)
