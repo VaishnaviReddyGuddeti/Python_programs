73e75c45a3a01bac72b1eab0312edafaae3f6390
VaishnaviReddyGuddeti <vaishnavireddyguddeti@gmail.com>
2020-12-02 19:34:37 +0530

    WhyPython.py

---
+++ Python_Introduction/WhyPython.py
@@ -0,0 +1,5 @@
+1.Python works on different platforms (Windows, Mac, Linux, Raspberry Pi, etc).
+2.Python has a simple syntax similar to the English language.
+3.Python has syntax that allows developers to write programs with fewer lines than some other programming languages.
+4.Python runs on an interpreter system, meaning that code can be executed as soon as it is written. This means that prototyping can be very quick.
+5.Python can be treated in a procedural way, an object-oriented way or a functional way.
