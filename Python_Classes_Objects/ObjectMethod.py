7718b9e83ab035aebff6b14c6f394a389a377161
VaishnaviReddyGuddeti <vaishnavireddyguddeti@gmail.com>
2020-12-02 18:21:56 +0530

    ObjectMethod.py

---
+++ Python_Classes_Objects/ObjectMethod.py
@@ -0,0 +1,11 @@
+# Insert a function that prints a greeting, and execute it on the p1 object:
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+
+p1 = Person("Vaishnavi",21)
+p1.myfunc()
