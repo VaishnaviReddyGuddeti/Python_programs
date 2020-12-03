02ef2fd29062cdeae8e2e10a3eee3bf346001cb3
VaishnaviReddyGuddeti <vaishnavireddyguddeti@gmail.com>
2020-12-03 14:45:43 +0530

    AddProperties1.py

---
+++ Python_Inheritance/AddProperties1.py
@@ -0,0 +1,16 @@
+# Add a year parameter, and pass the correct year when creating objects:
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+class Student(Person):
+  def __init__(self, fname, lname, year):
+    super().__init__(fname, lname)
+    self.graduationyear = year
+
+x = Student("Vaishnavi", "Reddy", 2020)
+print(x.graduationyear)
