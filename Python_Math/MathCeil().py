9526fcc93950de3c74fc39e82158895613fab682
VaishnaviReddyGuddeti <vaishnavireddyguddeti@gmail.com>
2020-12-08 13:58:31 +0530

    MathCeil().py

---
+++ Python_Math/MathCeil().py
@@ -0,0 +1,12 @@
+# The math.ceil() method rounds a number upwards to its nearest integer, and the math.floor() method rounds a number downwards to its nearest integer, and returns the result:
+#Import math library
+import math
+
+#Round a number upward to its nearest integer
+x = math.ceil(1.4)
+
+#Round a number downward to its nearest integer
+y = math.floor(1.4)
+
+print(x)
+print(y)
