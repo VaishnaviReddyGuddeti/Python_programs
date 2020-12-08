20d98e9a096f0061b67cfafdc327a6d8055bdf49
VaishnaviReddyGuddeti <vaishnavireddyguddeti@gmail.com>
2020-12-08 08:51:41 +0530

    ImportFromModule.py

---
+++ Python_Modules/ImportFromModule.py
@@ -0,0 +1,7 @@
+# You can choose to import only parts from a module, by using the from keyword.
+# The module named mymodule has one function and one dictionary:
+
+# Import only the person1 dictionary from the module:
+from mymodule import person1
+
+print(person1["age"])
