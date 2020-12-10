89fb38b117ccaddd5001695ad39f8205319685f4
VaishnaviReddyGuddeti <vaishnavireddyguddeti@gmail.com>
2020-12-10 20:38:11 +0530

    al{2}.py

---
+++ Python_RegEx/Metacharacters.py/al{2}.py
@@ -0,0 +1,15 @@
+# {}	Exactly the specified number of occurrences	"al{2}"
+import re
+
+txt = "The rain in Spain falls mainly in the plain!"
+
+#Check if the string contains "a" followed by exactly two "l" characters:
+
+x = re.findall("al{2}", txt)
+
+print(x)
+
+if x:
+  print("Yes, there is at least one match!")
+else:
+  print("No match")
