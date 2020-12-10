a402ea3e27e4e41a1b87c8c2782d0637546d6ea2
VaishnaviReddyGuddeti <vaishnavireddyguddeti@gmail.com>
2020-12-10 14:03:04 +0530

    findall.py

--- Python_RegEx/RegExFunctions.py/findall.py
+++ Python_RegEx/RegExFunctions.py/findall.py
@@ -7,3 +7,21 @@ import re
 txt = "The rain in Spain"
 x = re.findall("ai", txt)
 print(x)
+
+
+
+# If no matches are found, an empty list is returned:
+# Return an empty list if no match was found:
+import re
+
+txt = "The rain in Spain"
+
+#Check if "Portugal" is in the string:
+
+x = re.findall("Portugal", txt)
+print(x)
+
+if (x):
+  print("Yes, there is at least one match!")
+else:
+  print("No match")
