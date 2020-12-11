8e5a64af8a224ba6366b7ce694d904f2803e2595
VaishnaviReddyGuddeti <vaishnavireddyguddeti@gmail.com>
2020-12-11 11:45:49 +0530

    a-zA-Z.py

---
+++ Python_RegEx/Sets.py/a-zA-Z.py
@@ -0,0 +1,16 @@
+# Returns a match for any character alphabetically between a and z, lower case OR upper case
+
+import re
+
+txt = "8 times before 11:45 AM"
+
+#Check if the string has any characters from a to z lower case, and A to Z upper case:
+
+x = re.findall("[a-zA-Z]", txt)
+
+print(x)
+
+if x:
+  print("Yes, there is at least one match!")
+else:
+  print("No match")
