fea904c6619cfb005393eb657991d252329bb6dc
VaishnaviReddyGuddeti <vaishnavireddyguddeti@gmail.com>
2020-12-11 09:41:11 +0530

    b.py

---
+++ Python_RegEx/SpecialSequences.py/b.py
@@ -0,0 +1,34 @@
+# Returns a match where the specified characters are at the beginning or at the end of a word
+# (the "r" in the beginning is making sure that the string is being treated as a "raw string")	r"\bain"
+
+import re
+
+txt = "It is raining is UK"
+
+#Check if "ain" is present at the beginning of a WORD:
+
+x = re.findall(r"\bain", txt)
+
+print(x)
+
+if x:
+  print("Yes, there is at least one match!")
+else:
+  print("No match")
+
+# r"ain\b"
+
+import re
+
+txt = "The rain in spain"
+
+#Check if "ain" is present at the end of a WORD:
+
+x = re.findall(r"ain\b", txt)
+
+print(x)
+
+if x:
+  print("Yes, there is at least one match!")
+else:
+  print("No match")
