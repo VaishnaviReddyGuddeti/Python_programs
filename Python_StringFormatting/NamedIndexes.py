b022ba4e5ed03fb63fef335004dfb26916ac8d27
VaishnaviReddyGuddeti <vaishnavireddyguddeti@gmail.com>
2020-12-11 12:41:16 +0530

    NamedIndexes.py

---
+++ Python_StringFormatting/NamedIndexes.py
@@ -0,0 +1,5 @@
+# You can also use named indexes by entering a name inside the curly brackets {carname}, but then you must use names when you pass the parameter values txt.format(carname = "Ford"):
+
+
+myorder = "I have a {carname}, it is a {model}."
+print(myorder.format(carname = "Maserati", model = "Quattroporte"))
