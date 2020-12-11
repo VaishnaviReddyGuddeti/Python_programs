de791685cad962d059b965300307b22253135400
VaishnaviReddyGuddeti <vaishnavireddyguddeti@gmail.com>
2020-12-11 10:01:40 +0530

    W1.py

---
+++ Python_RegEx/SpecialSequences.py/W1.py
@@ -0,0 +1,16 @@
+# 	Returns a match where the string DOES NOT contain any word characters	"\W"
+
+import re
+
+txt = "The rain in India"
+
+#Return a match at every NON word character (characters NOT between a and Z. Like "!", "?" white-space etc.):
+
+x = re.findall("\W", txt)
+
+print(x)
+
+if x:
+  print("Yes, there is at least one match!")
+else:
+  print("No match")
